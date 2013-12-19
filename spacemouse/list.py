8bd8844e4acb39a60c10aa955ffe0e48c4e8d989
polyphemus <rolfmorel@gmail.com>
2013-12-19 17:27:47 +0100

    Base list_devices() on match_device()

--- spacemouse/list.py
+++ spacemouse/list.py
@@ -1,5 +1,5 @@
 from . import backend
-from .device import Device
+from .device import Device, match_device
 
 if backend.get('list') == "udev":
     from .udev.list import list_devices as backend_list_devices
@@ -11,9 +11,10 @@ device_list = []
 
 def list_devices():
     for devnode, manufacturer, product in backend_list_devices():
-        device = Device(devnode, manufacturer, product)
-
-        if device not in device_list:
-            device_list.append(device)
+        for device in device_list:
+            if match_device(device, devnode, manufacturer, product):
+                break
+        else:
+            device_list.append(Device(devnode, manufacturer, product))
 
     return device_list
