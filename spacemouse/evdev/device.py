fb0a90160e1e483586c2fc60d82bb1ad28729936
polyphemus <rolfmorel@gmail.com>
2013-12-20 00:25:18 +0100

    Try to properly silence __del__ exceptions

--- spacemouse/evdev/device.py
+++ spacemouse/evdev/device.py
@@ -27,8 +27,10 @@ class Device(object):
         return self._input_device == other._input_device
 
     def __del__(self):
-        if self._input_device is not None and hasattr(self, 'close'):
+        try:
             self.close()
+        except:
+            pass
 
     @property
     def fd(self):
